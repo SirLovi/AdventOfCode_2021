850dce68251136eade9f4b3b775e756c1a8a6efe
SirLovi <stanekdo@gmail.com>
2021-12-09 14:09:51 +0100

    Day 8 + Day 9

--- 8/8.py
+++ 8/8.py
@@ -1,25 +1,47 @@
 from os import path
+from itertools import permutations
 
 absolute_path = path.dirname(path.abspath(__file__))
 
 
 def solve(data):
 
-    med = data[len(data) // 2]
-    fuel_used1 = sum(abs(n - med) for n in data)
+    d = {
+        "abcefg": 0,
+        "cf": 1,
+        "acdeg": 2,
+        "acdfg": 3,
+        "bcdf": 4,
+        "abdfg": 5,
+        "abdefg": 6,
+        "acf": 7,
+        "abcdefg": 8,
+        "abcdfg": 9, }
 
-    mean = sum(data) // len(data)
-    fuel_used2 = sum(abs(n - mean) * (abs(n - mean) + 1) // 2 for n in data)
+    appearance = 0
+    summed = 0
+    for line in data:
+        a, b = line.split(" | ")
+        a = a.split()
+        b = b.split()
+        appearance += sum(len(code) in {2, 3, 4, 7} for code in b)
+        for permutation in permutations("abcdefg"):
+            to = str.maketrans("abcdefg", "".join(permutation))
+            a_ = ["".join(sorted(code.translate(to))) for code in a]
+            b_ = ["".join(sorted(code.translate(to))) for code in b]
+            if all(code in d for code in a_):
+                summed += int("".join(str(d[code]) for code in b_))
+                break
 
     print("\n1:")
-    print("Fuel used: ", fuel_used1)
+    print("Digits appeared x times: ", appearance)
 
     print("\n2:")
-    print("Fuel used: ", fuel_used2)
+    print("Values added up: ", summed)
 
 
 with open(absolute_path + "/input.txt", 'r') as file:
-    data = list(sorted(int(n) for n in file.read().split(",")))
+    data = file.readlines()
 
 
 # print(data)
--- 9/9.py
+++ 9/9.py
@@ -1,25 +1,53 @@
 from os import path
+from collections import Counter
 
 absolute_path = path.dirname(path.abspath(__file__))
 
 
 def solve(data):
 
-    med = data[len(data) // 2]
-    fuel_used1 = sum(abs(n - med) for n in data)
+    risk_sum = 0
+    local_min = 0
+    seen = {}
+    stack = []
+    for row in range(len(data)):
+        for position in range(len(data[0])):
 
-    mean = sum(data) // len(data)
-    fuel_used2 = sum(abs(n - mean) * (abs(n - mean) + 1) // 2 for n in data)
+            if all(
+                row + diff_row < 0
+                or row + diff_row >= len(data)
+                or position + diff_pos < 0
+                or position + diff_pos >= len(data[0])
+                or data[row][position] < data[row + diff_row][position + diff_pos]
+                for diff_row, diff_pos in ((0, -1), (0, 1), (-1, 0), (1, 0))
+            ):
+                risk_sum += 1 + data[row][position]
+
+            if (row, position) not in seen and data[row][position] != 9:
+                stack.append((row, position))
+                while stack:
+                    row, position = stack.pop()
+                    for diff_row, diff_pos in ((0, -1), (0, 1), (-1, 0), (1, 0)):
+                        r_ = row + diff_row
+                        c_ = position + diff_pos
+                        if 0 <= r_ < len(data) and 0 <= c_ < len(data[0]):
+                            if (r_, c_) not in seen and data[r_][c_] != 9:
+                                seen[(r_, c_)] = local_min
+                                stack.append((r_, c_))
+                local_min += 1
+
+    a, b, position = Counter(list(seen.values())).most_common(3)
+    part2 = (a[1] * b[1] * position[1])
 
     print("\n1:")
-    print("Fuel used: ", fuel_used1)
+    print("Risk sum: ", risk_sum)
 
     print("\n2:")
-    print("Fuel used: ", fuel_used2)
+    print("Largest basins multiplied: ", part2)
 
 
 with open(absolute_path + "/input.txt", 'r') as file:
-    data = list(sorted(int(n) for n in file.read().split(",")))
+    data = [list(map(int, line[:-1])) for line in file.readlines()]
 
 
 # print(data)
